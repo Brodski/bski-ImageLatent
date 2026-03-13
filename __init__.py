d462be0b8ba25919f6e64245e379aeb4e6850747
brodski <cbrodski@gmail.com>
2026-03-13 00:31:32 -0600

    init

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .bskinodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .bskinodes import comfy_entrypoint
+# __version__ = "0.1.0"
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+# __all__ = ["comfy_entrypoint"]
+
